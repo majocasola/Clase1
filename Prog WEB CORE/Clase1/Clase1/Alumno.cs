e4111e6053e1befaaef4525a445ab22934a1b97a
María José Casola <90106016+majocasola@users.noreply.github.com>
2023-05-22 18:13:55 -0300

    clase1

---
+++ Prog WEB CORE/Clase1/Clase1/Alumno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    internal class Alumno
+    {
+        //los campos, propiedades, metodos y eventos de una clase se llaman miembros de clase
+        // un objeto es la instancia de una clase
+        // campos / atributos que son privados
+        private int dni;
+        private string apellido;
+        private string nombre;
+        // propiedad que encapsula el dni:
+        // propiedades (encapsulamiento)
+        // get set lectura y escritura
+        public int DNI
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+        // encapsulo en apellido
+        // autoimplementado
+        public string Apellido {
+            get { return apellido; }
+            set { apellido = value.ToUpper(); }
+        }
+        public string Nombre { get; set; }
+        public string Email { get; set; }   
+
+        // metodos (en gral son publicos) Lo publico va en Mayus- lo privado en minisc
+        public string TraerDatos()
+        {
+            // doc:(nro) (apellido) (nombre)
+            string strDatos = $"DNI:{DNI} {Apellido}, {Nombre} Correo: {Email}";
+            return strDatos;
+        }
+
+        
+    }
+}
---
+++ Prog WEB CORE/Clase1/Clase1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // las clases en mayusc y singular
+
+            // crear una instancia de Alumno (no es estatica)
+            // nombre de clase y nombre del objeto para acceder a lo publico
+            Alumno alu1 = new Alumno();
+            alu1.DNI = 123456789;
+            alu1.Apellido = "Perez";
+            alu1.Nombre = "Juan";
+            alu1.Email = "perez@gmail.com";
+
+            // no se hace new de consola porque es estatica(todos sus miembros son estaticos)
+            //Console.WriteLine($"DNI: {alu1.DNI}");
+            Console.WriteLine($"Datos del alumno 1 : {alu1.TraerDatos()}");
+
+            // crear otra instancia de la clase de Alumno
+            Alumno alu2 = new Alumno();
+            alu2.DNI = 33654123;
+            alu2.Apellido = "Garcia";
+            alu2.Nombre = "Jose";
+            alu2.Email = "garcia@gmail.com";
+            Console.WriteLine($"\nDatos del alumno 2 : {alu2.TraerDatos()}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
